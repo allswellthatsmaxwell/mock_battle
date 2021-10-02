d3404aad1d170df9c6ff819922e981d1d1433ee0
Maxwell Peterson <allswellthatsmaxwell9@gmail.com>
2021-10-02 02:48:16 -0500

    got calibration and winning character incidence

--- matches.R
+++ matches.R
@@ -51,8 +51,6 @@ y <- list(trn=winners[indices$trn],
           val=winners[indices$val],
           tst=winners[indices$tst])
 
-max_depth = 4
-max_nodes <- 2^(max_depth - 1)
 params <- list(objective = "binary:logistic", eta = 0.05, alpha=3.5, lambda=0)
 dtrn <- xgb.DMatrix(data = X$trn, label=y$trn)
 dval <- xgb.DMatrix(data = X$val, label=y$val)
@@ -70,16 +68,63 @@ pROC::auc(y$val, pred$val)
 opponent_team <- c("Dire Druid", "Greenery Giant", "Phoenix Paladin", 
                    "Quartz Questant", "Tidehollow Tyrant")
 opponent_y <- sapply(columns, function(person) person %in% opponent_team) %>% as.numeric()
-possible_our_teams_mat <- greens_mat
-opponent_mat <- matrix(opponent_y, nrow=nrow(greens_mat), ncol=19, byrow=TRUE)
+possible_our_teams_mat <- apply(teams, 1, function(row) as.numeric(columns %in% row)) %>% t()
+opponent_mat <- matrix(opponent_y, nrow=nrow(possible_our_teams_mat), ncol=19, byrow=TRUE)
 possible_games_mat <- cbind(possible_our_teams_mat, opponent_mat)
 outcome_probas <- predict(model, possible_games_mat)
 possible_our_teams_dat <- data.frame(possible_our_teams_mat) %>% as_tibble()
 colnames(possible_our_teams_dat) <- columns
-possible_our_teams_dat$win_proba <- outcome_probas
+possible_our_teams_dat %<>% mutate(win_proba = outcome_probas) %>% arrange(desc(win_proba))
 top_n <- 10
-top_teams_dat <- possible_our_teams_dat %>% arrange(desc(win_proba)) %>% head(top_n)
+top_teams_dat <- possible_our_teams_dat %>% head(top_n)
+top_probas <- possible_our_teams_dat %$% win_proba %>% head(top_n)
+top_teams_dat %>% select(-win_proba) %>% apply(1, function(row) columns[c(row) == 1]) %>% t() %>% cbind(top_probas)
 
-permutations(n=19, r=5, v=columns, repeats.allowed = FALSE)
+scale_y_percent <- list(
+  scale_y_continuous(labels=partial(scales::percent, accuracy=1), limits=c(0, 1), breaks=seq(0, 1, 0.1)))
+theme_percent <- list(theme(panel.grid.minor = element_blank(), axis.title = element_text(size=14), 
+                            axis.text = element_text(size=12)))
 
-apply(top_teams_dat, 1, function(row) columns[c(row) == 1]) %>% t()
+possible_our_teams_dat %<>% dplyr::mutate(win_proba_above_cut = win_proba >= 0.7)
+possible_our_teams_dat %>% group_by(win_proba_above_cut) %>% summarize(n())
+above_cut_teams_dat <- possible_our_teams_dat %>% dplyr::filter(win_proba_above_cut)
+person_prop_above_cut_dat <- above_cut_teams_dat %>% 
+  .[columns] %>% 
+  apply(2, mean) %>% 
+  .[order(-.)] %>% 
+  tibble(name=names(.), proba=.)
+cat(person_prop_above_cut_dat %$% name %>% paste0(collapse="\n"))
+
+# Only 1500 of 11500 possible teams give a >= 0.50 model score against the blue team.
+# So they've got a pretty good team.
+# Check whether these probabilities are well-calibrated.
+all_win_probas_plot <- ggplot(possible_our_teams_dat) +
+  geom_line(aes(x=1:nrow(possible_our_teams_dat), y=win_proba[order(-win_proba)])) +
+  labs(x="Composition index", y="Win probability", 
+       title="Win probabilities for all possible Green compositions,\nagainst the specified Blue team.") + 
+  geom_hline(yintercept=0.5, color='blue', alpha=0.5) +
+  scale_y_percent +
+  scale_x_continuous(labels = function(x) paste0(x / 1000, "K"), breaks=seq(0, 12000, 1500)) +
+  theme_bw() +
+  theme_percent
+ggsave(all_win_probas_plot, path="results", filename = "all_win_probas.png", dpi=100, width=6, height=3)
+
+buckets <- list(val = round(pred$val * 100) %/% 10 * 10)
+buckets
+
+bucket_means_dat <- tibble(bucket = buckets$val, ytrue = y$val, ypred = pred$val) %>%
+  group_by(bucket) %>%
+  dplyr::summarize(true_win_prop = mean(ytrue), pred_win_prop = mean(ypred))
+calibration_plot <- bucket_means_dat %>%
+  ggplot(aes(x=true_win_prop, y=pred_win_prop)) +
+  geom_point(color='red', size=3) +
+  geom_abline(size=1) +
+  geom_line(color='red', size=1) +
+  theme_bw() +
+  scale_y_percent +
+  scale_x_continuous(labels=partial(scales::percent, accuracy=1), limits=c(0, 1), breaks=seq(0, 1, 0.1)) +
+  theme_percent +
+  labs(x="true win %", y="predicted win %", title="Calibration")
+
+ggsave(calibration_plot, path="results", filename = "calibration.png", dpi=100, width=6, height=3)
+         
